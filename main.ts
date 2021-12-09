24ac655fabc2ac03bb6446ab229530d04cbbb5f6
edouard3443454 <93147216+edouard3443454@users.noreply.github.com>
2021-12-09 13:17:53 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,14 @@ let a = 0
 a = 0
 basic.forever(function () {
     if (a == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(2000)
         a = 0
     } else {
         pins.digitalWritePin(DigitalPin.P0, 1)
